5d894c64f04a6d1385ac3c632604048c6d91a4a1
bernardip <61267487+bernardip@users.noreply.github.com>
2020-09-17 19:41:17 +1000

    MainActivity changes

--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
@@ -3,12 +3,16 @@ package au.edu.unsw.infs3634.covidtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "HelloThere";
+    private int age = 21;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate Line 14: Age = " + age);
         setContentView(R.layout.activity_main);
     }
 }
