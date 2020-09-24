64eba0bb84bc0adcb33b488fc9b62694a14e96b9
bernardip <61267487+bernardip@users.noreply.github.com>
2020-09-24 18:41:54 +1000

    MainActivity changes

--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
@@ -2,8 +2,11 @@ package au.edu.unsw.infs3634.covidtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = "HelloThere";
@@ -14,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate Line 14: Age = " + age);
         setContentView(R.layout.activity_main);
+
+        //Add a button to the DetailActivity layout
+        // Find the launch button on the view
+        Button btn = findViewById(R.id.btnLaunchDetail);
+        //btn.setText("random");
+
+        //implement setOnClickListener
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
